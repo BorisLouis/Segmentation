7372c74b8713b57aa452b999ffb90329c8bdff23
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2019-03-19 13:42:29 +0100

    Added explanation as to how to use the code, change the order of the running sections for user convenience

--- plotPoreProps.m
+++ plotPoreProps.m
@@ -1,18 +1,48 @@
+% The aim of this code is to plot CCDF and histograme for the properties 
+% previously calculated by mainPoreSizeCalc.m. It was coded to be
+% relatively flexible so one can plot any property extracted
+
+% HOW TO USE:
+% In loading input the user provide information about the folder in which
+% the data he wants to process is (typically PoreSize-Results if one is
+% using our workflow.
+% Then the user needs to provide info about the dimension of the analysis
+% results he/she wants to plot (2D or 3D)and the pore property (volume,
+% area, extRad, inRad, throats,...)
+
+% The code will output a figure with 2 subplot, a CCDF and an Histogram and
+% one with... (ADD SUSANA's CODE)
+
+% Note: The order of the section allow the user to change the property
+% to be plotted without having to load the data again by running the code
+% section per section (shift+enter on the targeted section).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% AUTHOR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Boris Louis (https://github.com/BorisLouis)                             %
+% Rafael Camacho Dejay (https://github.com/CamachoDejay)                  %
+% Susana Rocha (https://github.com/SusanaRocha)                           %
+%                                                                         %
+% Website : Rafael Camacho Dejay: https://camachodejay.github.io/         %
+%           Boris Louis:  https://borislouis.github.io/                   %
+%           Susana Rocha: https://susanarocha.github.io/                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+
 clear;
 clc;
 close all;
+%% Loading input
+fileExt = '.mat';
+outputName = 'Figures';
+[file2Analyze,currentFolderName,outDir] = Load.Folder(fileExt,outputName);
 %% user input
 nBins = 100;
 dim   = '3D';
 prop2Plot = 'connect';
-fileExt = '.mat';
-outputName = 'Figures';
-%% Load 
-[file2Analyze,currentFolderName,outDir] = Load.Folder(fileExt,outputName);
+%% Prepare Data to be plotted
 data2Plot = Load.getData2Plot(file2Analyze,dim,prop2Plot);
 fields = fieldnames(data2Plot);
 nFile  = length(fields);
-%% GetCDF
+%% Get the CCDF
 for i = 1 : nFile
     
     currField = fields{i};
