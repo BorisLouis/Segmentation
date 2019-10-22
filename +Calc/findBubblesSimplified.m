d2ada332e2f660eab9b183e76867a1c7fb55da9a
DESKTOP-RTJSFL7\Boris <boris.louis999@gmail.com>
2019-10-22 18:26:04 +0200

    Started to code the bubble analysis to answer reviewer

---
+++ +Calc/findBubblesSimplified.m
@@ -0,0 +1,26 @@
+function [bubble_radii, bubble_coord] = findBubblesSimplified(img)
+% find_bubbles determines the location and radii of 2D or 3D bubbles in
+% the binary image img.
+%
+% 19|03|2013 Stefan Münster
+EDM = bwdist(img); % This calculation of the EDM of the fluid phase assumes
+%that fluid pixels are 0 and solid pixels are 1;
+smoothed_EDM = imfilter(EDM,fspecial('gaussian',5,1)); % this smoothing
+%suppresses bubbles of similar size in close proximity and can be omitted
+local_maxima = imregionalmax(smoothed_EDM); % determines the local maxima,
+%which are the center locations of the bubbles
+
+if(length(size(img))<3) % translates local maxima into x,y,z coordinates
+    
+ [bubble_coord(:,1),bubble_coord(:,2)]=...
+     ind2sub(size(img),find(local_maxima));
+ 
+else
+    
+ [bubble_coord(:,1), bubble_coord(:,2),bubble_coord(:,3)]=...
+     ind2sub(size(img),find(local_maxima));
+ 
+end
+bubble_radii=EDM(local_maxima); % determines the radii of the bubbles from
+%the EDM values at the local maxima
+end
